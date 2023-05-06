daf19d787fecd9d634b6c299236092192cf83c98
Brianna Tedlock <briannatedlock02@gmail.com>
2023-05-06 16:00:33 -0500

    initial commit

--- scripts.js
+++ scripts.js
@@ -1,2 +1,29 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+ window.addEventListener("load", function() {
+let takeOffbutton = document.getElementById("takeOff");
+
+button.addEventListener("click", takeOff);
+    target.style.backgroundColor = "blue";
+}); 
+ 
+
+    function myFunction() {
+        alert("Are you sure shuttle is ready for takeoff.");
+        let response = confirm("Confirm that shuttle is ready for takeoff");
+        if (response) {
+          console.log("Confirm that shuttle is ready for takeoff");
+        } else {
+          console.log("...");
+        };
+      };
+
+      let button = document.getElementById("takeOff");
+        button.addEventListener("click", takeOff);
+
+        function takeOff() {
+        document.getElementById("flightStatus").innerHTML = "Shuttle in flight";
+          console.log("Shuttle in flight");
+        }
+        
+      
